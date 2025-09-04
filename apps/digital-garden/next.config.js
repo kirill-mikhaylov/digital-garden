9f122cd9c589f0a9d41bbabef7114100ce794959
Kirill Mikhaylov <320758+kirill-mikhaylov@users.noreply.github.com>
2025-09-03 17:11:35 -0700

    chore: modified nextjs git workflow

--- apps/digital-garden/next.config.js
+++ apps/digital-garden/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   distDir: "dist",
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
